b2c49059dcefddc6d2c47cc9c8aed24e4ae168db
swishyy <swishydesu@outlook.com>
2022-01-21 18:05:13 +0000

    Changed tab spacing

--- main.py
+++ main.py
@@ -2,104 +2,104 @@ from util.findNth import find_nth
 
 def fullTextSearch(target, arr, mode):
 
-	if mode == "c" or mode == "w" or mode == "i" or mode == "wi":
-		pass
-	else:
-		raise Exception("Invalid mode.")
+    if mode == "c" or mode == "w" or mode == "i" or mode == "wi":
+        pass
+    else:
+        raise Exception("Invalid mode.")
 
-	foundIndexes = []
-	foundWords = []
-	foundCount = 0
-		
-	for word in arr:
+    foundIndexes = []
+    foundWords = []
+    foundCount = 0
 
-		if len(word) < len(target):
-			pass
-		else:
+    for word in arr:
 
-			brokenWord = list(word)
-			seen = []
+        if len(word) < len(target):
+            pass
+        else:
 
-			for x in range(len(word)):
-				if len(word) == len(target):
-					if word == target and word not in foundWords:
-						foundCount+=1
-						foundWords.append(word)
-				elif len(word) > len(target):
-					for char in brokenWord:
-						try:
+            brokenWord = list(word)
+            seen = []
 
-							if seen.count(char) > 1:
+            for x in range(len(word)):
+                if len(word) == len(target):
+                    if word == target and word not in foundWords:
+                        foundCount+=1
+                        foundWords.append(word)
+                elif len(word) > len(target):
+                    for char in brokenWord:
+                        try:
 
-								result = [item for item in seen if item == char]
-								count = len(result)
+                            if seen.count(char) > 1:
 
-								for x in range(count):
+                                result = [item for item in seen if item == char]
+                                count = len(result)
 
-									indexOfRepeat = find_nth(word, char, x)
-									nextChar = indexOfRepeat + 1
+                                for x in range(count):
 
-									currentString = brokenWord[indexOfRepeat] + brokenWord[nextChar]
+                                    indexOfRepeat = find_nth(word, char, x)
+                                    nextChar = indexOfRepeat + 1
 
-									if currentString == target and word not in foundWords:
-										foundCount+=1
-										foundWords.append(word)
+                                    currentString = brokenWord[indexOfRepeat] + brokenWord[nextChar]
 
-									seen.append(char)
+                                    if currentString == target and word not in foundWords:
+                                        foundCount+=1
+                                        foundWords.append(word)
 
-							else:
-								nextChar = brokenWord.index(char) + 1
-								currentString = char + brokenWord[nextChar]
+                                    seen.append(char)
 
-								if currentString == target and word not in foundWords:
-									foundCount+=1
-									foundWords.append(word)
+                            else:
+                                nextChar = brokenWord.index(char) + 1
+                                currentString = char + brokenWord[nextChar]
 
-								seen.append(char)
+                                if currentString == target and word not in foundWords:
+                                    foundCount+=1
+                                    foundWords.append(word)
+
+                                seen.append(char)
 
 
-						except IndexError:
-							lastChar = brokenWord.index(char) - 1
-							currentString = brokenWord[lastChar] + char
+                        except IndexError:
+                            lastChar = brokenWord.index(char) - 1
+                            currentString = brokenWord[lastChar] + char
 
-							if currentString == target and word not in foundWords:
-								foundCount+=1
-								foundWords.append(word)
+                            if currentString == target and word not in foundWords:
+                                foundCount+=1
+                                foundWords.append(word)
 
-							seen.append(char)
+                            seen.append(char)
 
-						#finally:
-							# Debug: print(f"char: {char}, currentString: {currentString}")
+                        #finally:
+                            # Debug: print(f"char: {char}, currentString: {currentString}")
 
-	if foundWords:
+    if foundWords:
 
-		indexes = []
+        indexes = []
 
-		for word in foundWords:
-			indexes.append(arr.index(word))
+        for word in foundWords:
+            indexes.append(arr.index(word))
 
-	if mode == "c":
-		return foundCount
-	elif mode == "w":
-		return foundWords
-	elif mode == "i":
-		
-		if indexes:
-			return indexes
-		else:
-			return "No matches found."
+    if mode == "c":
+        return foundCount
+    elif mode == "w":
+        return foundWords
+    elif mode == "i":
 
-	elif mode == "wi":
-		
-		if foundWords:
-			if indexes:
-				return foundWords, indexes
-			else:
-				return foundWords
-		else:
-			return "No matches found."
+        if indexes:
+            return indexes
+        else:
+            return "No matches found."
 
-	print(f"foundCount: {foundCount}, foundWords: {foundWords}")
+    elif mode == "wi":
 
-# Example 
+        if foundWords:
+            if indexes:
+                return foundWords, indexes
+            else:
+                return foundWords
+        else:
+            return "No matches found."
+
+    print(f"foundCount: {foundCount}, foundWords: {foundWords}")
+
+# Example
 # print(fullTextSearch('hi', ['hello', 'hi', 'hi there', 'hills', 'bonjour', 'a'], "wi"))
